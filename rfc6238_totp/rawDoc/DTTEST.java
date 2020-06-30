e771d12c8ea63474bf79ea76def7a9c6026309d7
fwx-gitrepo3-test5 <fwx5618177@gmail.com>
2020-06-30 16:59:05 +0900

    successful test TOTP algorithm, and get the result based time

--- rfc6238_totp/rawDoc/DTTEST.java
+++ rfc6238_totp/rawDoc/DTTEST.java
@@ -9,7 +9,7 @@ import javax.crypto.spec.SecretKeySpec;
 // import java.nio.ByteBuffer;
 // import java.util.*;
 import java.math.BigInteger;
-import java.util.TimeZone;
+// import java.util.TimeZone;
 import java.util.Base64;
 
 
@@ -188,12 +188,12 @@ public static String generateTOTP(String key,
     }
     
 
-    public static String stringToAscii(String value)  
+    public static String stringToHexAscii(String value)  
     {  
         StringBuffer sbu = new StringBuffer();  
         char[] chars = value.toCharArray();   
         for (int i = 0; i < chars.length; i++) {  
-                sbu.append(Integer.valueOf(chars[i]).intValue());  
+                sbu.append(Integer.toHexString(Integer.valueOf(chars[i]).intValue()));  
 
         }  
         return sbu.toString();  
@@ -212,6 +212,25 @@ public static String generateTOTP(String key,
         }  
         return sbu.toString();  
     }
+
+    /*
+        整体思路：
+        总结：TOTP算法对userid和passwd加密，然后post到指定host;
+            1.原始字符串转换成ASCII码的数字表示；
+            2.ASCII码的数字转换为16进制；
+            3.取原子时的时钟戳，去掉时区。在转换时，考虑int和long int转换的小数舍弃情况；
+            4.设置30S动态时间口令；
+            5.HmacSHA512加密；
+            6.返回结果标准化处理(userid:passwd-timebasedResult)；
+            7.对标准化结果进行base64编码；
+            8.Postman的headers增加内容——Authorization: Basic base64编码标准结果；
+            9.Content-Type: application/json;
+            10.Body->raw->content:
+                {
+                    "contact_email": "",
+                    "github_url": ""
+                }
+    */
      public static void main(String[] args) {
         while(true){
         long T0 = 0;
@@ -219,28 +238,36 @@ public static String generateTOTP(String key,
         // String steps = "0";
         String seed = "q506169874@gmail.com";
         String postString = "HENNGECHALLENGE003";
-        String asciiResult = stringToAscii(seed + postString);
+        // 通常编码，要将字符串转换编码，然后换成16进制编码；
+        // 一般编程里面，没有特殊要求，编码都是十六进制的
+        String asciiResult = stringToHexAscii(seed + postString);
         // String asciiResult = "3530363136393837344071712e636f6d48454e4e47454348414c4c454e4745303033";
         // String asciiResult = stringToBase64(seed + postString);
         String stringResult = asciiToString(asciiResult);
 
         DateFormat df = new SimpleDateFormat("YYYY-MM-dd HH:mm:ss");
-        df.setTimeZone(TimeZone.getTimeZone("JST"));
+        // df.setTimeZone(TimeZone.getTimeZone("JST"));
 
-
-        int testTime = (int) (System.currentTimeMillis());
+        // 此处除以1000，是因为float和int转换会丢弃小数位
+        // 多数编程语言都有一个缺点，两个整数相除。如果结果是小数，最后得出的结果就是一个整数。
+        // 小数位会被丢弃，所以利用这一点，除以30之前，根本不需要做整除的处理。
+        // 浮点的长度普遍比整型长。
+        // 这个计算结果这样，其实是c语言导致的，有浮点数参与计算，结果才可以为浮点。
+        // 数值计算你可以记一个经典例子。1/2=0，1/2.0=0.5，1.0/2=0.5
+        int testTime = (int) (System.currentTimeMillis()/1000L);
         int reminderTime =X - (int) (System.currentTimeMillis() / 1000 % X);
         
-        long T = (testTime - T0) / X / 1000 ;
+        long T = (testTime - T0) / X ;
         
         String steps = Long.toHexString(T).toUpperCase();
-        // System.out.println("steps:"+T);
+        System.out.println("除以30后的时间:"+T);
         while (steps.length() < 16) steps = "0" + steps;
 
 
         System.out.println(df.format(new Date(System.currentTimeMillis())));
-        System.out.println("unix time:"+testTime);
-        
+        System.out.println("unix time（正确）:"+testTime);
+        System.out.println("unix time2（原始时间）:"+(System.currentTimeMillis()));
+        System.out.println("unix time2（Int转型截取数据）:"+(int) (System.currentTimeMillis()));
         System.out.println("Ascii Result:" + asciiResult);
         System.out.println("stringResult:" + stringResult);
         System.out.println("HmacSHA512:"+generateTOTP(asciiResult, steps, "10", "HmacSHA512"));
@@ -253,7 +280,7 @@ public static String generateTOTP(String key,
         while(reminderTime > 0){
             try {
                 
-                System.out.print(reminderTime);
+                System.out.print(reminderTime+",");
                 Thread.sleep(1000);
                 reminderTime--;
             }catch (Exception ex){
